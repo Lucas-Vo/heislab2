e67ea293b3f086ea0f13f746a14db0e8dedb3909
Lucas Vo <lv@vitalthings.com>
2026-01-17 15:38:04 +0100

    written networkthread.go

---
+++ App/common/utils.go
@@ -0,0 +1,33 @@
+package common
+
+func TrimZeros(b []byte) []byte {
+	i := len(b)
+	for i > 0 && b[i-1] == 0 {
+		i--
+	}
+	return b[:i]
+}
+
+func CopyElevState(st ElevState) ElevState {
+	cp := st
+	if st.CabRequests != nil {
+		cp.CabRequests = make([]bool, len(st.CabRequests))
+		copy(cp.CabRequests, st.CabRequests)
+	}
+	return cp
+}
+
+func DeepCopyNetworkState(ns NetworkState) NetworkState {
+	out := NetworkState{
+		HallRequests: nil,
+		States:       make(map[string]ElevState, len(ns.States)),
+	}
+	if ns.HallRequests != nil {
+		out.HallRequests = make([][2]bool, len(ns.HallRequests))
+		copy(out.HallRequests, ns.HallRequests)
+	}
+	for k, st := range ns.States {
+		out.States[k] = CopyElevState(st)
+	}
+	return out
+}
--- App/main.go
+++ App/main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
-	
 
 	// Adjust these imports to your actual module path:
 	//"elevator/elevnetwork"
+	"context"
 	. "elevator/common"
-
+	"os"
+	"os/signal"
 	//quic "github.com/quic-go/quic-go"
 )
 
 func main() {
+	// ctrl + c handling
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+	go func() {
+		<-sig
+		cancel()
+	}()
+
 	// filip til lucas
 	elevalgoServiced := make(chan NetworkState)
 	elevalgoLaManana := make(chan NetworkState)
+
+	// lucas til filip
+	snapshotToFSM := make(chan NetworkState)
 
 	// lucas til vetle
 	networkStateOfTheWorld := make(chan NetworkState)
@@ -22,7 +37,11 @@ func main() {
 	// vetle til filip
 	assignerOutput := make(chan ElevInput)
 
-	go networkthread(elevalgoServiced,elevalgoLaManana,networkStateOfTheWorld,theWorldIsReady)
-	go assignerThread(networkStateOfTheWorld,theWorldIsReady,assignerOutput)
-	go fsmthread(assignerOutput,elevalgoServiced,elevalgoLaManana)
+	cfg := DefaultConfig()
+
+	go networkthread(ctx, cfg, elevalgoServiced, elevalgoLaManana, networkStateOfTheWorld, theWorldIsReady, snapshotToFSM)
+	go assignerThread(networkStateOfTheWorld, theWorldIsReady, assignerOutput)
+	go fsmthread(assignerOutput, elevalgoServiced, elevalgoLaManana, snapshotToFSM)
+
+	<-ctx.Done()
 }
