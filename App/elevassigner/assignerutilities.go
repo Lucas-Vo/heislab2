e8150e1ae4b174f92301a74d29e6ed37d65d4224
OscarBratli <racsodimmen@gmail.com>
2026-01-19 20:44:07 +0100

    Added the shi

--- App/elevassigner/assignerutilities.go
+++ App/elevassigner/assignerutilities.go
@@ -1,13 +1,13 @@
 package elevassigner
 
 import (
-	
 	. "elevator/common"
-
 )
 
 const HRA_EXECUTABLE = "hall_request_assigner" // Linux only
 
+// RemoveStaleStates removes the elevator states for the nodes that are marked as stale.
+// It mutates networkSnapshot by deleting entries from networkSnapshot.States.
 func RemoveStaleStates(networkSnapshot *NetworkState) {
 	for id, alive := range networkSnapshot.Alive {
 		if !alive {
--- README.md
+++ README.md
@@ -4,4 +4,6 @@ files that have fully used chat no hesitation:
 Spørsmål til anders
 
 Kan det oppstå at heis 1 har kontakt med heis 2, heis 2 med heis 3, men ikke heis 1 med heis 2?
+Utgjør alle aktive heiser en clique
+
 
