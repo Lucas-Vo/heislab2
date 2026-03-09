1d69cb5191bfcf7ddc791b79d0f126fc44a9de3d
Lucas Vo <lv@vitalthings.com>
2026-03-09 21:15:23 +0100

    pansexualna 3

--- App/common/config.go
+++ App/common/config.go
@@ -20,7 +20,7 @@ func DefaultConfig() (Config, string, error) {
 		HostByID: map[int]string{
 			1: "10.100.23.32",
 			2: "10.100.23.29",
-			//3: "10.100.23.37",
+			3: "10.100.23.172",
 			4: "192.168.0.197", // filip ip
 			5: "10.22.135.140", // veetel ip
 			6: "10.24.64.186",  // lucas ip
