23926990d13c206145cc44223ed97d1eee10b11d
Vetlebrur <vetlerod@gmail.com>
2026-03-10 14:53:30 +0100

    BIHH

--- App/common/config.go
+++ App/common/config.go
@@ -17,12 +17,12 @@ func DefaultConfig() (Config, string, error) {
 	config := Config{
 		Ports: []int{4242, 4243},
 		HostByID: map[int]string{
-			1: "10.100.23.34",
-			2: "10.100.23.35",
-			3: "10.100.23.37",
+			1: "10.100.23.20",
+			2: "10.100.23.22",
+			// 3: "10.100.23.37",
 			//4: "192.168.0.197", // filip ip
 			//5: "10.22.135.140", // veetel ip
-			6: "10.24.64.186", // lucas ip
+			// 6: "10.24.64.186", // lucas ip
 		},
 	}
 	if err := config.initSelf(); err != nil {
