29eec1a77460e46e08fa538ed185667f35b4a8b3
Sanntidssal plass 18 <sanntid@finnesikke.no>
2026-03-16 20:48:02 +0100

    Synchronizer? barely even know her

--- App/elevfsm/synchronizer.go
+++ App/elevfsm/synchronizer.go
@@ -23,9 +23,16 @@ func NewFsmSync(config common.Config) *Synchronizer {
 }
 
 func (sync *Synchronizer) HandleNetworkSnapshot(snapshot common.Snapshot, now time.Time) {
-	_, sync.hasAlivePeer = common.RemoveDeadStates(&snapshot, sync.selfKey)
+	sync.hasAlivePeer = false
 	sync.coherent = snapshot.Coherent
-
+	for key, alive := range snapshot.Alive {
+		if key != sync.selfKey && alive {
+			if _, hasState := snapshot.States[key]; hasState {
+				sync.hasAlivePeer = true
+				break
+			}
+		}
+	}
 	for floor := range common.N_FLOORS {
 		sync.netRequests[floor][0] = snapshot.HallRequests[floor][0]
 		sync.netRequests[floor][1] = snapshot.HallRequests[floor][1]
