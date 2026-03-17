c7936059d07bf35d2e6c4ff7c68d4020e641bc88
Charlotte-schr <charsc@stud.ntnu.no>
2026-03-17 20:55:11 +0100

    Config

--- App/common/config.go
+++ App/common/config.go
@@ -26,9 +26,9 @@ func DefaultConfig() (Config, error) {
 		PeerPort: 4242,
 		MsgPort:  4243,
 		HostByKey: map[int]string{
-			1: "10.100.23.30",
+			1: "10.100.23.28",
 			2: "10.100.23.32",
-			3: "10.100.23.172",
+			3: "10.100.23.33",
 		},
 	}
 	if err := config.initSelf(); err != nil {
